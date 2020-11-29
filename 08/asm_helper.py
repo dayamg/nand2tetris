2d66fd7f1aa997860b19898d3b6b10d9e8b9f99c
dayamg <68069418+dayamg@users.noreply.github.com>
2020-11-29 15:11:48 +0200

    Add files via upload

---
+++ 08/asm_helper.py
@@ -0,0 +1,292 @@
+
+ADD_ASM = """
+/// adds *SP + *(SP-1)
+@R0
+AM=M-1  /// go to *SP
+D=M /// D = y
+
+/// SP--
+@R0
+AM=M-1  /// go to --SP
+D=M+D /// D = x + y
+
+@R0
+A=M  /// go to *SP
+M=D  /// keep the result of add in *SP
+
+@R0
+M=M+1  /// SP++
+"""
+
+SUB_ASM = """
+/// subtracts *SP - *(SP-1), x - y
+@R0
+AM=M-1  /// go to *SP
+D=M /// D = y
+
+/// SP--
+@R0
+AM=M-1  /// go to --SP, M = x
+D=M-D /// D = x - y
+
+@R0
+A=M  /// go to *SP
+M=D  /// keep the result of add in *SP
+
+@R0
+M=M+1  /// SP++
+"""
+
+GT_ASM = """
+/// returns 1 iff *SP < *(SP-1), and 0 otherwise, , i.e., true iff y < x
+@R0
+AM=M-1  /// go to *SP
+D=M /// D=y
+
+@R13
+M=D /// R[13]=y
+
+@YNEGGTi
+D;JLT /// if D<0, jump to y negative (YNEGGT)
+
+@R15 /// else, keep sign of D as +
+M=1
+
+(PARSEXGTi)
+/// SP--
+@R0
+AM=M-1  /// go to --SP
+D=M /// D=*SP=x
+@R14
+M=D /// R[14]=x
+
+@XNEGGTi
+D;JLT /// if x<0, jump to y negative (XNEGGT)
+
+@R15
+MD=M-1  /// sign of x: 1, so calculate R15 = sign(y)-sign(x), D = sign(y)-sign(x)
+
+(CHECKSIGNGTi)
+@RETURNFALSEGTi
+D;JGT /// if D = sign(y)-sign(x) > 0, then y > x, so return FALSE
+
+@RETURNTRUEGTi
+D;JLT /// if D = sign(y)-sign(x) < 0, then y < x, so return TRUE
+
+/// Else, we need to compare x and y, and as they are the same sign, there are no overflow problems
+@R14
+D=M  /// D = x
+@R13 /// M = y
+D=M-D /// D = y - x
+
+@RETURNFALSEGTi
+D;JGT /// if D = y - x > 0, then y > x, so return FALSE
+
+@RETURNFALSEGTi
+D;JEQ /// if D = y - x = 0, then y = x, so return FALSE
+
+@RETURNTRUEGTi
+D;JLT /// if D = y - x < 0, then y < x, so return TRUE
+@ENDGTi
+0;JMP
+
+(YNEGGTi)
+@R15 /// keep R15 = sign of y = -1
+M=-1
+@PARSEXGTi
+0;JMP /// continue parsing the value of x
+
+(XNEGGTi)
+@R15
+MD=M+1  /// sign of x: -1, so calculate R15 = sign(y)-sign(x)
+@CHECKSIGNGTi
+0;JMP /// next: check if both are with the same sign
+
+(RETURNFALSEGTi)
+@R0
+A=M
+M=0
+@ENDGTi
+0;JMP
+
+(RETURNTRUEGTi)
+@R0
+A=M
+M=-1
+
+(ENDGTi)
+@R0
+M=M+1  /// SP++
+"""
+
+LT_ASM = """
+/// returns 1 iff *SP < *(SP-1), and 0 otherwise, , i.e., true iff y < x
+@R0
+AM=M-1  /// go to *SP
+D=M /// D=y
+
+@R13
+M=D /// R[13]=y
+
+@YNEGLTi
+D;JLT /// if D<0, jump to y negative (YNEG)
+
+@R15 /// else, keep sign of D as +
+M=1
+
+(PARSEXLTi)
+/// SP--
+@R0
+AM=M-1  /// go to --SP
+D=M /// D=*SP=x
+@R14
+M=D /// R[14]=x
+
+@XNEGLTi
+D;JLT /// if x<0, jump to y negative (XNEGLTi)
+
+@R15
+MD=M-1  /// sign of x: 1, so calculate R15 = sign(y)-sign(x), D = sign(y)-sign(x)
+
+(CHECKSIGNLTi)
+@RETURNTRUELTi
+D;JGT /// if D = sign(y)-sign(x) > 0, then y > x, so return FALSE
+
+@RETURNFALSELTi
+D;JLT /// if D = sign(y)-sign(x) < 0, then y < x, so return TRUE
+
+/// Else, we need to compare x and y, and as they are the same sign, there are no overflow problems
+@R14
+D=M  /// D = x
+@R13 /// M = y
+D=M-D /// D = y - x
+
+@RETURNTRUELTi
+D;JGT /// if D = y - x > 0, then y > x, so return FALSE
+
+@RETURNFALSELTi
+D;JEQ /// if D = y - x = 0, then y = x, so return FALSE
+
+@RETURNFALSELTi
+D;JLT /// if D = y - x < 0, then y < x, so return TRUE
+@ENDLTi
+0;JMP
+
+(YNEGLTi)
+@R15 /// keep R15 = sign of y = -1
+M=-1
+@PARSEXLTi
+0;JMP /// continue parsing the value of x
+
+(XNEGLTi)
+@R15
+MD=M+1  /// sign of x: -1, so calculate R15 = sign(y)-sign(x)
+@CHECKSIGNLTi
+0;JMP /// next: check if both are with the same sign
+
+(RETURNFALSELTi)
+@R0
+A=M
+M=0
+@ENDLTi
+0;JMP
+
+(RETURNTRUELTi)
+@R0
+A=M
+M=-1
+
+(ENDLTi)
+@R0
+M=M+1  /// SP++
+"""
+
+NOT_ASM = """
+/// returns not y
+@R0
+A=M-1 /// go to *SP
+M=!M /// *SP = not *SP
+"""
+
+OR_ASM = """
+/// returns y or x (bitwise)
+@R0
+AM=M-1  /// go to *--SP
+D=M /// D = y
+
+/// SP--
+@R0
+AM=M-1  /// go to SP-1
+D=D|M /// D = x or y
+
+@R0
+A=M
+M=D /// *SP = x|y
+
+@R0
+M=M+1  /// go to SP++
+"""
+
+AND_ASM = """
+/// returns y and x (bitwise)
+@R0
+AM=M-1  /// go to *--SP
+D=M /// D = y
+
+/// SP--
+@R0
+AM=M-1  /// go to SP-1
+D=D&M /// D = x or y
+
+@R0
+A=M
+M=D /// *SP = x|y
+
+@R0
+M=M+1  /// go to SP++
+"""
+
+NEG_ASM = """
+/// neg *SP
+@R0
+A=M-1 /// go to *--SP
+M=-M /// D=*SP
+"""
+
+EQ_ASM = """
+/// checks *(SP-1) == *(SP-2), i.e., y==x
+@R0
+AM=M-1  /// go to --SP
+D=M /// D = y
+
+/// SP--
+@R0
+AM=M-1  /// go to --SP
+D=D-M /// D = y-x
+
+@TRUEEQi
+D;JEQ /// if D == 0, jump to return TRUE
+@R0
+A=M
+M=0 /// else, return FALSE
+@ENDEQi
+0;JMP
+
+(TRUEEQi)
+@R0
+A=M
+M=-1 /// return true
+
+(ENDEQi)
+@R0
+M=M+1  /// SP++
+"""
+
+IF_GOTO_ASM = """
+@R0
+AM=M-1
+D=M
+@label_name
+D;JNE
+"""
+
---
+++ 08/vm_translator.py
@@ -0,0 +1,248 @@
+import os
+import re
+import sys
+from asm_helper import *
+
+VM_SUFFIX = ".vm"
+ASM_SUFFIX = ".asm"
+READ_MODE = "r"
+WRITE_MODE = "w"
+NEW_LINE = '\n'
+SPACE_CHAR = " "
+
+CONSTANT = "constant"
+
+A_COMMAND_PREFIX = '@'
+
+SP_ADDRESS = 0
+LCL_ADDRESS = 1
+ARG_ADDRESS = 2
+THIS_ADDRESS = 3
+THAT_ADDRESS = 4
+
+SP_CHAR = "SP"
+
+g_index = 0  # Global index for labeling
+
+# A dictionary for translating variables types. Notice: the string "SP" is not used in the vm file
+# (as there is no special name for it), and "constant" is not a real RAM section
+SEGMENT_DICT = {SP_CHAR: SP_ADDRESS, CONSTANT: SP_ADDRESS, "local": LCL_ADDRESS, "argument": ARG_ADDRESS,
+                "this": THIS_ADDRESS, "that": THAT_ADDRESS}
+POINTER_DICT = {0: THIS_ADDRESS, 1: THAT_ADDRESS}
+STARTING_ADDRESS_DICT = {"temp": 5, "static": 16, "stack": 256}
+
+ARITHMETIC_DICT = {"add": ADD_ASM, "neg": NEG_ASM, "sub": SUB_ASM, "eq": EQ_ASM, "lt": LT_ASM,
+                   "gt": GT_ASM, "not": NOT_ASM, "or": OR_ASM, "and": AND_ASM}
+
+
+def generate_label_cmd(vm_cmd, func_name):
+    label_name = vm_cmd[1]
+    cmd_string = "(" + label_name + ")"
+    if func_name:
+        cmd_string = "(" + str(func_name) + "$" + label_name + ")"
+    return cmd_string
+
+
+def generate_goto_cmd(vm_cmd, func_name):
+    label_name = vm_cmd[1]
+    cmd_string = "@" + label_name + "\n" + "0;JMP"
+    if func_name:
+        cmd_string = "@" + str(func_name) + "$" + label_name + "\n" + "0;JMP"
+    return cmd_string
+
+
+def generate_if_goto_cmd(vm_cmd, func_name):
+    label_name = vm_cmd[1]
+    label_cmd = label_name
+    if func_name:
+        label_cmd = str(func_name) + "$" + label_name
+
+    cmd_string = IF_GOTO_ASM
+    cmd_string = cmd_string.replace("label_name", label_cmd)
+    return cmd_string
+
+
+def generate_push_cmd(vm_cmd, vm_file):
+    """
+    writes the command "push segment i" in asm and return it.
+    """
+    segment = vm_cmd[1]
+    cmd_string = ""
+
+    if segment in ["local", "argument", "this", "that"]:
+        # address = segmentPointer+i, *SP = *address, SP++
+        cmd_string = "@R?\nD=M\n@i\nA=D+A\nD=M // D = segment i\n@R0\nA=M\nM=D // *SP=D\n@R0\nM=M+1"
+        seg_addr = str(SEGMENT_DICT[segment])
+        cmd_string = cmd_string.replace("?", seg_addr)
+
+    if segment == "static":
+        cmd_string = "@name.i\nD=M\n@R0\nA=M\nM=D\n@R0\nM=M+1"
+
+    if segment == "constant":
+        # *sp=i, sp++
+        cmd_string = "@i\nD=A\n@R0\nA=M\nM=D\n@R0\nM=M+1"
+
+    if segment == "temp":
+        # address = 5+i, *sp=*address, sp++
+        cmd_string = "@5\nD=A\n@i\nA=D+A \nD=M // D = segment i\n@R0\nA=M\nM=D // *SP=D\n@R0\nM=M+1"
+
+    index = vm_cmd[2]
+    cmd_string = cmd_string.replace("i", str(index))
+    cmd_string = cmd_string.replace("name", vm_file)  # For static commands
+
+    if segment == "pointer":
+        # *sp=THIS/THAT, sp++
+        cmd_string = "@R?\nD=M\n@R0\nA=M\nM=D	// *sp= R3/4\n@R0\nM=M+1"
+        # if index is 0 then: THIS-3 else if 1 then: THAT-4
+        cmd_string = cmd_string.replace("?", str(POINTER_DICT[index]))
+    return cmd_string
+
+
+def generate_pop_cmd(vm_cmd, vm_file):
+    """
+    writes the command "pop segment i" in asm and return it.
+    """
+    segment = vm_cmd[1]
+    cmd_string = ""
+
+    if segment in ["local", "argument", "this", "that"]:
+        # addr = segmentPointer + i, SP - -, *addr = *SP
+        cmd_string = "@R?\nD=M\n@i\nD=D+A\n@R13 // R13 = segment i addr\nM=D\n@R0\nAM=M-1\nD=M\n@R13\nA=M\nM=D"
+        seg_addr = str(SEGMENT_DICT[segment])
+        cmd_string = cmd_string.replace("?", seg_addr)
+
+    if segment == "static":
+        cmd_string = "@R0\nAM=M-1\nD=M // D = stack.pop\n@name.i\nM=D"
+
+    if segment == "temp":
+        # address=5+i, sp--, *address=*sp
+        cmd_string = "@5\nD=A\n@i\nD=D+A\n@R13 // R13 = addr of segment i\nM=D\n@R0\nAM=M-1\nD=M\n@R13\nA=M\nM=D"
+
+    index = vm_cmd[2]
+    cmd_string = cmd_string.replace("i", str(index))
+    cmd_string = cmd_string.replace("name", vm_file)  # For static commands
+
+    if segment == "pointer":
+        # sp--, THIS/THAT=*sp
+        cmd_string = "@R0\nAM=M-1\nD=M\n@R?\nM=D"
+        # if index is 0 then: THIS-3 else if 1 then: THAT-4
+        cmd_string = cmd_string.replace("?", str(POINTER_DICT[index]))
+    return cmd_string
+
+
+def write_vm_cmd_to_asm(vm_cmd, asm_file, vm_file):
+    """
+    find the vm command type, generate it in asm, and write to asm file.
+    """
+    global g_index
+
+    # Write the translated command in a comment in the asm file.
+    cmd_string = "///// "
+    for i in vm_cmd:
+        cmd_string += " " + str(i)
+    asm_file.write(cmd_string + NEW_LINE)
+
+    # Extract the file name for push/pop static commands.
+    file_name = os.path.splitext(os.path.basename(vm_file.name))[0]
+
+    cmd_type = vm_cmd[0]
+    if cmd_type == "push":
+        asm_cmd = generate_push_cmd(vm_cmd, file_name)
+
+        # Write cmd_string to asm file.
+        asm_file.write(asm_cmd + NEW_LINE)
+
+    if cmd_type == "pop":
+        asm_cmd = generate_pop_cmd(vm_cmd, file_name)
+
+        # Write cmd_string to asm file.
+        asm_file.write(asm_cmd + NEW_LINE)
+
+    if cmd_type in ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]:
+        arithmetic_asm_str = ARITHMETIC_DICT[cmd_type].replace("i", str(g_index))
+        asm_file.write(arithmetic_asm_str + NEW_LINE)
+        g_index += 1
+
+
+def remove_comments_and_spaces(segment):
+    """
+    Removes inline comments and spaces
+    (Are there even comments in vm?)
+    """
+    pattern = re.compile(r"\s+")  # remove spaces
+    segment = re.sub(pattern, '', segment)
+    pattern = re.compile(r"//.*")  # remove comments
+    segment = re.sub(pattern, '', segment)
+    return segment
+
+
+def remove_comments(segment):
+    """
+    Removes inline comments and spaces
+    (Are there even comments in vm?)
+    """
+    pattern = re.compile(r"//.*")  # remove comments
+    segment = re.sub(pattern, '', segment)
+    return segment
+
+
+def split_line_into_segments(vm_line):
+    """
+    Divides a line into a list of commands,
+    e.g., "push local 0" is translated into ["push", "local", 0].
+    Removes spaces between.
+    Converts string representing numbers, into integers (e.g., "0" to 0).
+    """
+    segments_array = vm_line.split(SPACE_CHAR)
+    segments_array = list(filter(None, segments_array))
+
+    # Remove spaces:
+    for i in range(len(segments_array)):
+        segments_array[i] = remove_comments_and_spaces(segments_array[i])
+
+        # Convert numbers to int
+        if str(segments_array[i]).isdigit():
+            segments_array[i] = int(segments_array[i])
+
+    return segments_array
+
+
+def vm_translator(vm_path, asm_file):
+    """
+    Get vm and asm files path, and translate vm into asm file.
+    """
+    vm_file = open(vm_path, READ_MODE)
+
+    for line in vm_file:
+
+        line = remove_comments(line)
+        # if line is only whitespace or empty, then skip it.
+        if line.isspace() or line == "":
+            continue
+
+        vm_cmd = split_line_into_segments(line)
+        write_vm_cmd_to_asm(vm_cmd, asm_file, vm_file)
+
+    vm_file.close()
+
+
+if __name__ == "__main__":
+    vm_path_input = sys.argv[1]
+    # Check if a file or a directory of vm files.
+    if os.path.isfile(vm_path_input):
+        asm_path = vm_path_input.replace(VM_SUFFIX, ASM_SUFFIX)
+        asm_file = open(asm_path, WRITE_MODE)
+        vm_translator(vm_path_input, asm_file)
+        asm_file.close()
+
+    if os.path.isdir(vm_path_input):
+        vm_path_input = vm_path_input.rstrip('/')
+        asm_file_name = os.path.basename(vm_path_input)
+        asm_path = os.path.join(vm_path_input, asm_file_name+ASM_SUFFIX)
+        asm_file = open(asm_path, WRITE_MODE)
+        for filename in os.listdir(vm_path_input):
+            if filename.endswith(VM_SUFFIX):
+                # Write a comment with file name
+                asm_file.write("////////*****//////// " + filename + NEW_LINE)
+                vm_translator(os.path.join(vm_path_input, filename), asm_file)
+        asm_file.close()
